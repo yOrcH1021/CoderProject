c284d6652ed0d026afaabd27117b0032319ed1c6
Jorge Olaya <edonkey122@hotmail.com>
2021-08-03 17:45:53 -0500

    add more files

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+
+// TOOLTIP
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
